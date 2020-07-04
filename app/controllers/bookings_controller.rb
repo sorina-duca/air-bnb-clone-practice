ba820501198e818f318637701d9a4fd56df901de
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-04 16:50:50 +0200

    not finished yet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,35 @@
 class BookingsController < ApplicationController
+  before_action :find_booking, only: [ :edit, :destroy]
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.boat = @boat
+    if @booking.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to booking_path(@booking.user)
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:content)
+  end
+
 end
