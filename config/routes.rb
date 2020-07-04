2155b587a4eecc170fab5f92e10bca6235c44ba6
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-04 22:41:42 +0200

    :users routes changed to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  resources :users, only: %i[show edit update destroy] do
+
+  resources :users, only: %i[show] do
     resources :boats, only: %i[index show new create edit update]
     resources :bookings, only: %i[index show new create edit update]
   end
+
   resources :boats, only: %i[destroy]
   resources :bookings, only: %i[destroy]
+
 end
