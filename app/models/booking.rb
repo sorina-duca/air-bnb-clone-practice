5cc762374175ffe5920908f9504d233530352fb7
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-05 00:44:39 +0200

    add validation to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
  belongs_to :users
  belongs_to :boats
+
+ validates :checkin, :checkout, presence: { message: "Please select the date."}
+ validates :booking_price, presence: true
 end
