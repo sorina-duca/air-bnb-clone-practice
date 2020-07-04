5498ecddecb500d26ededb72fb42af8d87664ab0
Sorina Duca <sorina.duca@gmail.com>
2020-07-04 16:01:21 +0200

    change data type of description to text in boat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_135331) do
+ActiveRecord::Schema.define(version: 2020_07_04_135941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boats", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.integer "capacity"
     t.string "location"
     t.integer "price"
