5cd8aebf35c7f36e5f6d9ced5072e169d4a05301
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-04 19:57:58 +0200

    added columns checkin, checkout, status, booking-price to the table bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_135941) do
+ActiveRecord::Schema.define(version: 2020_07_04_174301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
