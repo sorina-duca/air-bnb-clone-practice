ccc9807f6ced6396eaab4f5613f9e7b0d9d7523e
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-07 20:30:03 +0200

    delete validation of booking_price

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
  belongs_to :boats
 
  validates :checkin, :checkout, presence: { message: "Please select the date."}
- validates :booking_price, presence: true
 end
