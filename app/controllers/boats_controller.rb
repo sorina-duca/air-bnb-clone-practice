17830137cd2fdd2681f30e066f336189d72a6341
Sorina Duca <sorina.duca@gmail.com>
2020-07-07 20:10:29 +0200

    create boats controller with simple views, without pundit

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,49 @@
+class BoatsController < ApplicationController
+  before_action :find_boat, only: [ :edit, :update, :show, :destroy ]
+
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    @boat.user = current_user
+    @boat.save
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @boat.update(boat_params)
+    redirect_to boat_path(@boat)
+  end
+
+  def destroy
+    @boat.destroy
+    # to change when we create a user page
+    redirect_to root_path
+  end
+
+  private
+
+  def find_boat
+    @boat = Boat.find(params[:id])
+  end
+
+  def boat_params
+    params.require(:boat).permit(:name, :description, :capacity, :location, :price, :type)
+  end
+end
