801fbdee75b85c11e7f72bd50fd7eb9fe6cd5878
Mateusz Abramczyk <mateusz.abramczyk95@wp.pl>
2020-07-07 19:55:46 +0200

    Added active storage, cloudinary

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,6 @@
 class Boat < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
   validates :name, presence: { message: 'You must pick the name of your boat' }
   validates :description, presence: true, length: { minimum: 100 }
   validates :capacity, presence: true, inclusion: { in: [2, 4, 6],
