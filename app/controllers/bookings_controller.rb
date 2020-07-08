97a2bd3bfc159d90b044b374028498ee201467b7
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-09 01:31:35 +0200

    make booking_policy.rb file

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,23 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :edit, :update]
-  before_action :set_boat, only: [:new, :create]
+  before_action :find_booking, only: [:show, :edit, :update]
+  before_action :find_boat, only: [:new, :create]
   before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @bookings = Booking.all
+    @bookings = policy(Booking).order(name: :asc)
   end
 
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
-    @booking.user = current_user.id
-    @booking.boat = params[:boat_id]
+    @booking.user = current_user
+    @booking.boat = Boat.find(params[:boat_id])
+    authorize @booking
     if @booking.save
       redirect_to booking_path(@booking)
     else
@@ -23,19 +26,21 @@ class BookingsController < ApplicationController
   end
 
   def show
+    authorize @booking
   end
 
   def edit
+    authorize @booking
   end
 
   def update
     @booking.update(booking_params)
-    redirect_to user_booking_path(@booking.user)
+    redirect_to booking_path(@booking)
   end
 
   private
 
-  def set_booking
+  def find_booking
     @booking = Booking.find(params[:id])
   end
 
@@ -43,7 +48,7 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:checkin, :checkout, :status, :booking_price)
   end
 
-  def set_boat
+  def find_boat
     @boat = Boat.find(params[:boat_id])
   end
 end
