b0539368b8cc5f41d4d23bf167d3b43c6b7fe491
Sorina Duca <sorina.duca@gmail.com>
2020-07-10 21:29:17 +0200

    add disabling of dates already booked

--- app/javascript/custom/datepickr.js
+++ app/javascript/custom/datepickr.js
@@ -1,6 +1,13 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const dates = document.getElementById('disable-unavailable-dates');
+const unavailable = JSON.parse(dates.dataset.dates);
+
+
 flatpickr('.datepicker', {
-  altInput: true
+  altInput: true,
+    dateFormat: "Y-m-d",
+    disable: unavailable
 });
+
