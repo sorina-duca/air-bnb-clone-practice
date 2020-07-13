414932d30ded468e55a48c935b9e119bf8054baa
Chihoko Ueno <harrierraum@hotmail.com>
2020-07-13 21:57:53 +0200

    use symbol

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
       resources :bookings, only: %i[new create show edit update]
   end
 
-  resources :bookings, only: %i[index]
+  resources :bookings, only: :index
 end
