fdc4aeec05e4e033cd415a41ec97447f146679b5
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-14 11:00:46 +0200

    reviews views finish, review grid in show boat page done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,22 +3,29 @@ class ReviewsController < ApplicationController
 
   def new
     @reviews = Review.new
+    authorize @reviews
   end
 
   def create
     @review = Review.new(review_params)
     @review.boat = @boat
-    @review.save
-    redirect_to boat_path(@boat)
+    @review.user = current_user
+    authorize @review
+    if @review.save
+      redirect_to boat_path(@boat)
+    else
+      render :new
+    end
   end
 
   private
 
   def find_boat
     @boat = Boat.find(params[:boat_id])
+    authorize @boat
   end
 
   def review_params
-    params.require(:review).permite(:content, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 end
