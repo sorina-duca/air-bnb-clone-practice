90328976d277a58c0d1b85bd14a48963c37ef48a
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-16 19:51:50 +0200

    adding default picture when user no upload a picture

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,15 +2,16 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
+  validates :name, presence: { message: 'You must pick the name of your boat' }
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
   has_many :reviews, dependent: :destroy
+  # uncomment after development of controller
 
   # validates :description, presence: true, length: { minimum: 100 }
-  validates :name, presence: { message: 'You must pick the name of your boat' }
-  validates :capacity, presence: true, inclusion: { in: [2, 4, 6],
-                                                    message: "Please choose the capacity" }
+  # validates :capacity, presence: true, inclusion: { in: [2, 4, 6],
+  #                                              message: "Please choose the capacity" }
   validates :location, presence: true
-  validates :price, presence: true, numericality: { message: 'must be a number' }
-  validates :category, presence: true, inclusion: { in: ['motorboat', 'sailboat'] }
+  # validates :price, presence: true, numericality: { message: 'must be a number' }
+  # validates :category, presence: true, inclusion: { in: ['motorboat', 'sailboat'] }
 end
